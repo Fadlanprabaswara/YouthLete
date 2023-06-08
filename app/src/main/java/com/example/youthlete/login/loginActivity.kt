c305e172a794123ca4e373bd0a6f3368a308493e
Fadlanprabaswara <prabaswarafadlan@gmail.com>
2023-06-08 15:40:31 +0700

    memperbaiki layout login dan register

--- app/src/main/java/com/example/youthlete/login/loginActivity.kt
+++ app/src/main/java/com/example/youthlete/login/loginActivity.kt
@@ -1,12 +1,22 @@
 package com.example.youthlete.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.youthlete.R
+import com.example.youthlete.databinding.ActivityLoginBinding
+import com.example.youthlete.register.RegisterActivity
 
 class loginActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+
+        binding.tvRegister.setOnClickListener{
+            val intent = Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
