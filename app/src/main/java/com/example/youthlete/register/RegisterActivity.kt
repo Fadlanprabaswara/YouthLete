ff973ce7ec15cba19a9ac2315bbff5b9cfc80529
Fadlanprabaswara <prabaswarafadlan@gmail.com>
2023-06-09 17:10:13 +0700

    memperbaiki layout login

--- app/src/main/java/com/example/youthlete/register/RegisterActivity.kt
+++ app/src/main/java/com/example/youthlete/register/RegisterActivity.kt
@@ -2,11 +2,14 @@ package com.example.youthlete.register
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.youthlete.R
+import com.example.youthlete.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
