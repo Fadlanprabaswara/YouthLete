433a58553ebae30121e381a97457435d3be28538
Raiji17 <Raihananoji8@gmail.com>
2023-06-13 08:56:49 +0700

    nambahin detail

---
+++ app/src/main/java/com/example/youthlete/Detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.youthlete.Detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.youthlete.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
