ac71c93400a7f0452af8924cec79ead9c6590648
Fadlanprabaswara <prabaswarafadlan@gmail.com>
2023-06-13 18:31:58 +0700

    menambahkan data di sportlist, serta menambahkan gambar di drawable

--- app/src/main/java/com/example/youthlete/model/sportList.kt
+++ app/src/main/java/com/example/youthlete/model/sportList.kt
@@ -28,13 +28,59 @@ object sportList {
         ),
         Sports(
             "Renang",
-            "50",
+            "3",
             R.drawable.swimming_image,
             " Renang merupakan satu di antara cabang olahraga akuatik. Renang adalah upaya untuk menggerakkan (mengapungkan atau mengangkat) semua bagian tubuh ke atas permukaan air.",
             listOf(
                 Course("Pengenalan Renang"),
                 Course("Teknik Dasar Renang"),
                 Course("Gaya Kupu-kupu")
+            )
+        ),
+        Sports(
+            "Fighter",
+            "6",
+            R.drawable.sports_boxing,
+            "Seni bela diri campuran atau lebih dikenal dengan sebutan Mixed Martial Arts (MMA) adalah olahraga kontak yang memperbolehkan berbagai teknik pertarungan, seperti pergumulan, tendangan, dan pukulan.Di dalam MMA, masing-masing praktisi didorong untuk mengkombinasikan teknik dari berbagai cabang seni bela diri untuk melumpuhkan lawan",
+            listOf(
+                Course("Abductors"),
+                Course("Adductors"),
+                Course("Biceps"),
+                Course("Calves"),
+                Course("Chest"),
+                Course("Foreams"),
+            )
+        ),
+        Sports(
+            "Basketball",
+            "8",
+            R.drawable.sports_basketball,
+            "Bola basket merupakan salah satu jenis olahraga yang menggunakan bola besar. Bola dimainkan secara berkelompok yang terdiri dari dua tim yang masing-masing beranggotakan lima orang. Kedua tim tersebut bersaing untuk mencetak poin sebanyak-banyaknya dengan cara memasukkan bola ke ring/basket lawan",
+            listOf(
+                Course("Abductors"),
+                Course("Adductors"),
+                Course("Biceps"),
+                Course("Calves"),
+                Course("Chest"),
+                Course("Glutes"),
+                Course("Hamstring"),
+                Course("Lower_Back"),
+            )
+        ),
+        Sports(
+            "Volleyball",
+            "8",
+            R.drawable.sports_volley,
+            "permainan olahraga yang dimainkan oleh dua grup berlawanan. Masing-masing grup memiliki enam orang pemain. Terdapat pula variasi permainan bola voli pantai yang masing-masing timnya hanya memiliki dua orang pemain.",
+            listOf(
+                Course("Abductors"),
+                Course("Adductors"),
+                Course("Biceps"),
+                Course("Calves"),
+                Course("Chest"),
+                Course("Shoulder"),
+                Course("Hamstring"),
+                Course("Glutes"),
             )
         )
     )
